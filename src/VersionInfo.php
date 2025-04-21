679c9837a5eae97e50e6797ee10e5874e7f29e92
Dries C <coenendries3@gmail.com>
2025-04-21 15:37:13 +0200

    Rename name in VersionInfo, closes #532

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -30,7 +30,7 @@ use function is_int;
 use function str_repeat;
 
 final class VersionInfo{
-	public const NAME = "PocketMine-MP";
+	public const NAME = "NG-PocketMine-MP";
 	public const BASE_VERSION = "5.27.2";
 	public const IS_DEVELOPMENT_BUILD = true;
 	public const BUILD_CHANNEL = "stable";
