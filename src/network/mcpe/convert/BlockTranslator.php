6052e77f8e33284768cc33a7a816117088dc485c
Dries C <15795262+dries-c@users.noreply.github.com>
2025-08-11 02:18:17 +0200

    Add 1.21.100 support

--- src/network/mcpe/convert/BlockTranslator.php
+++ src/network/mcpe/convert/BlockTranslator.php
@@ -46,17 +46,21 @@ final class BlockTranslator{
 			self::CANONICAL_BLOCK_STATES_PATH => '',
 			self::BLOCK_STATE_META_MAP_PATH => '',
 		],
+		ProtocolInfo::PROTOCOL_1_21_93 => [
+			self::CANONICAL_BLOCK_STATES_PATH => '-1.21.93',
+			self::BLOCK_STATE_META_MAP_PATH => '-1.21.93',
+		],
 		ProtocolInfo::PROTOCOL_1_21_90 => [
-			self::CANONICAL_BLOCK_STATES_PATH => '',
-			self::BLOCK_STATE_META_MAP_PATH => '',
+			self::CANONICAL_BLOCK_STATES_PATH => '-1.21.93',
+			self::BLOCK_STATE_META_MAP_PATH => '-1.21.93',
 		],
 		ProtocolInfo::PROTOCOL_1_21_80 => [
-			self::CANONICAL_BLOCK_STATES_PATH => '',
-			self::BLOCK_STATE_META_MAP_PATH => '',
+			self::CANONICAL_BLOCK_STATES_PATH => '-1.21.93',
+			self::BLOCK_STATE_META_MAP_PATH => '-1.21.93',
 		],
 		ProtocolInfo::PROTOCOL_1_21_70 => [
 			self::CANONICAL_BLOCK_STATES_PATH => '-1.21.70',
-			self::BLOCK_STATE_META_MAP_PATH => '',
+			self::BLOCK_STATE_META_MAP_PATH => '-1.21.70',
 		],
 		ProtocolInfo::PROTOCOL_1_21_60 => [
 			self::CANONICAL_BLOCK_STATES_PATH => '-1.21.60',
